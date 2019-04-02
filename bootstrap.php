a67c971c7c70af3ceec31890375e6f84349cf599
Eughen <dev@inventure.com.ua>
2019-04-02 16:21:58 +0300

    add bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+define( 'SKYNAR_DEBUG', true);
+define( 'SKYNAR_TIMING', true);
+define( 'SKYNAR_MODULE', realpath(__DIR__.'/mod'));
+
+require_once realpath(realpath(__DIR__).'/cms/actual/global.php');
+
