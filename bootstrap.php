9fb51a694c56615ffd1ce896915034a1d7152ede
Eughen <dev@inventure.com.ua>
2019-04-02 16:25:14 +0300

    clear debug from production

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
-define( 'SKYNAR_DEBUG', true);
-define( 'SKYNAR_TIMING', true);
+if(!defined('SKYNAR_DEBUG')){ define( 'SKYNAR_DEBUG', false); }
+if(!defined('SKYNAR_TIMING')){ define( 'SKYNAR_TIMING', false); }
 define( 'SKYNAR_MODULE', realpath(__DIR__.'/mod'));
 
 require_once realpath(realpath(__DIR__).'/cms/actual/global.php');
