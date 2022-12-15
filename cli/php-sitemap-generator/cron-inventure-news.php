f6f34265bfe28d4d20910f6392507631795c8f77
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-12-16 00:23:22 +0200

    Improved cli/php-sitemap-generator/cron-inventure-news.php

--- cli/php-sitemap-generator/cron-inventure-news.php
+++ cli/php-sitemap-generator/cron-inventure-news.php
@@ -10,23 +10,27 @@ $yourSiteUrl = 'https://inventure.com.ua';
 // The generator writes output to the current directory by default.
 $outputDir = getcwd();
 
-$generator = new \Icamys\SitemapGenerator\SitemapGenerator($yourSiteUrl, $outputDir);
+// $generator = new \Icamys\SitemapGenerator\SitemapGenerator($yourSiteUrl, $outputDir);
 
 // Create a compressed sitemap
-$generator->enableCompression();
+// $generator->enableCompression();
 
 // Determine how many urls should be put into one file;
 // this feature is useful in case if you have too large urls
 // and your sitemap is out of allowed size (50Mb)
 // according to the standard protocol 50000 urls per sitemap
 // is the maximum allowed value (see http://www.sitemaps.org/protocol.html)
-$generator->setMaxUrlsPerSitemap(50000);
+// $generator->setMaxUrlsPerSitemap(50000);
 
 // Set the sitemap file name
-$generator->setSitemapFileName("news-sitemap.xml");
+// $generator->setSitemapFileName("news-sitemap.xml");
 
 // Set the sitemap index file name
-$generator->setSitemapIndexFileName("news-sitemap-index.xml");
+// $generator->setSitemapIndexFileName("news-sitemap-index.xml");
+
+$sitemapXml = '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">';
 
 // For each language
 $langs = ['en', 'uk', 'ru'];
@@ -43,21 +47,36 @@ foreach ($langs as $lang) {
                         $lastMod = new DateTime($n1['lastmod']);
                     }
                     $location = str_replace($yourSiteUrl . $yourSiteUrl, $yourSiteUrl, $n1['loc']);
-                    $generator->addURL($location, $lastMod, null, null, null);
+                    // $generator->addURL($location, $lastMod, null, null, null);
+                    $sitemapXml .= '  <url>
+   <loc>' . $location . '</loc>
+   <news:news>
+   <news:publication>
+     <news:name>The Example Times</news:name>
+     <news:language>' . $lang . '</news:language>
+   </news:publication>
+   <news:publication_date>' . $lastMod->format('Y-m-d') . '</news:publication_date>
+     <news:title>Companies A, B in Merger Talks</news:title>
+    </news:news>
+  </url>';
                 }
             }
         }
     }
 }
+$sitemapXml .= '</urlset>';
+if (file_put_contents($outputDir . '/news-sitemap.xml', $sitemapXml)) {
+    echo ' Wrote news sitemap to file: ' . $outputDir . '/news-sitemap.xml' . PHP_EOL;
+}
 
 // Flush all stored urls from memory to the disk and close all necessary tags.
-$generator->flush();
+// $generator->flush();
 
 // Move flushed files to their final location. Compress if the option is enabled.
-$generator->finalize();
+// $generator->finalize();
 
 // Update robots.txt file in output directory or create a new one
-$generator->updateRobots();
+// $generator->updateRobots();
 
 // Submit your sitemaps to Google, Yahoo, Bing and Ask.com
-$generator->submitSitemap();
+// $generator->submitSitemap();
