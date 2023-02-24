fc7b4d98a8fcd225a1001e2fb53faa3eff0438f9
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2023-02-24 21:13:02 +0200

    Update settings.php
    Updated Hubspot API key

--- cli/config/settings.php
+++ cli/config/settings.php
@@ -6,7 +6,7 @@
  * Date: 16.08.2018
  * Time: 14:05
  *
- * Version: 2020-09-15 19:38
+ * Version: 2023-02-24 21:12
  *
  * @author Taras Shkodenko <taras@shkodenko.com>
  */
@@ -23,8 +23,9 @@ global $settings;
 $settings = [
     '23' => [ // 074bmom3utuln3yym6joh12dnl6ow6lltiark1ar
 	'hubspot' => [
-	       // 'api_key' => 'a541de91-9b17-48f3-95f2-2d9715bca884', // @ts dev
-	       'api_key' => 'fe2ed9e5-a69f-430e-8056-ddef0ee7c77c', // InVenture production
+	       // 'api_key' => 'a541de91-9b17-48f3-95f2-2d9715bca884',         // @ts dev
+	       // 'api_key' => 'fe2ed9e5-a69f-430e-8056-ddef0ee7c77c',         // InVenture production old API Key
+		   'api_key' => 'pat-na1-42969d3e-8acf-493b-8a02-b5d7b37a7309',    // InVenture production
 	   ],
 	   'google_offline_source' => 'UA-42378215-1',
     ],
