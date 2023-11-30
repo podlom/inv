eaa82bf160eff34949040d8ab01931a522adf2ee
Taras Shkodenko <shkodenko.t@novaposhta.ua>
2023-11-30 16:57:54 +0200

    Refactored ip-info.php

--- www/ip-info.php
+++ www/ip-info.php
@@ -32,7 +32,39 @@ function ipInfo($ip = null, $purpose = 'location', $deep_detect = true) {
         if (isset($ipdat->geoplugin_countryCode) && strlen($ipdat->geoplugin_countryCode) == 2) {
             $output = null;
             switch ($purpose) {
-                // cases for each purpose
+                case "location":
+                    $output = array(
+                        "city" => @$ipdat->geoplugin_city,
+                        "state" => @$ipdat->geoplugin_regionName,
+                        "country" => @$ipdat->geoplugin_countryName,
+                        "country_code" => @$ipdat->geoplugin_countryCode,
+                        "continent" => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+                        "continent_code" => @$ipdat->geoplugin_continentCode
+                    );
+                    break;
+                case "address":
+                    $address = array($ipdat->geoplugin_countryName);
+                    if (@strlen($ipdat->geoplugin_regionName) >= 1)
+                        $address[] = $ipdat->geoplugin_regionName;
+                    if (@strlen($ipdat->geoplugin_city) >= 1)
+                        $address[] = $ipdat->geoplugin_city;
+                    $output = implode(", ", array_reverse($address));
+                    break;
+                case "city":
+                    $output = @$ipdat->geoplugin_city;
+                    break;
+                case "state":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "region":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "country":
+                    $output = @$ipdat->geoplugin_countryName;
+                    break;
+                case "countrycode":
+                    $output = @$ipdat->geoplugin_countryCode;
+                    break;
             }
             return $output;
         }
@@ -41,8 +73,10 @@ function ipInfo($ip = null, $purpose = 'location', $deep_detect = true) {
 }
 
 $ipToCheck = $_SERVER['REMOTE_ADDR'];
-if (isset($_GET['ip']) && filter_var($_GET['ip'], FILTER_VALIDATE_IP)) {
-    $ipToCheck = $_GET['ip'];
+// Use FILTER_VALIDATE_IP to validate and sanitize the input.
+$userIP = filter_input(INPUT_GET, 'ip', FILTER_VALIDATE_IP);
+if ($userIP) {
+    $ipToCheck = $userIP;
 }
 
 echo var_export(ipInfo($ipToCheck), true);
