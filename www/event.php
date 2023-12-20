d553bd90ef6bca449c943cad63f90f8df15c2293
Taras Shkodenko <shkodenko.t@novaposhta.ua>
2023-12-20 16:08:53 +0200

    Enable debug log by IP in event.php

--- www/event.php
+++ www/event.php
@@ -19,7 +19,7 @@ function l_m($msg)
     }
     // IP: 193.0.217.7 - Kyiv - Volodymyra Ivasyuka 24-a
     // IP: 176.37.192.192 - Kyiv - Rollhouse cafe
-    if (is_writeable($logFileName) && isset($_SERVER['HTTP_CF_CONNECTING_IP']) && ($_SERVER['HTTP_CF_CONNECTING_IP'] == '176.37.192.192')) {
+    if (is_writeable($logFileName) && isset($_SERVER['HTTP_CF_CONNECTING_IP']) && ($_SERVER['HTTP_CF_CONNECTING_IP'] == '193.0.217.7')) {
         error_log(date('r') . ' ' . $msg . PHP_EOL, 3, $logFileName);
     }
     // Do not log on prod by default
