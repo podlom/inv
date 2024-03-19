6b7a9c1c8eae4fb579130598c207a91b0f1b6bc5
Taras Shkodenko <shkodenko.t@novadigital.com>
2024-03-19 23:04:27 +0200

    Improved categories match in news.php

--- www/news.php
+++ www/news.php
@@ -95,7 +95,7 @@ if (!empty($_REQUEST)) {
                 $msg = __FILE__ . ' +' . __LINE__ . ' @ts $path: ' . var_export($path19, true) . '; $category: ' . var_export($category, true);
                 l_m($msg);
                 //
-                if (preg_match('|^' . preg_quote($path19, '|') . '/(.*)?|', $parsedHttpReferer['path'], $m19)) {
+                if (preg_match('|^' . preg_quote($path19, '|') . '(/.*)?$|', $parsedHttpReferer['path'], $m19)) {
                     $msg = __FILE__ . ' +' . __LINE__ . ' @ts $m19: ' . var_export($m19, true);
                     l_m($msg);
                     //
