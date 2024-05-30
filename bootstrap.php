d4fc8cbb82d7601d115cd19ce8e60475f238fab1
Taras Shkodenko <shkodenko.t@novadigital.com>
2024-05-30 10:39:59 +0300

    Added vendor packages autoload into bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,12 @@
 <?php
-if(!defined('SKYNAR_DEBUG')){ define( 'SKYNAR_DEBUG', false); }
-if(!defined('SKYNAR_TIMING')){ define( 'SKYNAR_TIMING', false); }
-define( 'SKYNAR_MODULE', realpath(__DIR__.'/mod'));
 
-require_once realpath(realpath(__DIR__).'/cms/actual/global.php');
+if (!defined('SKYNAR_DEBUG')) {
+    define('SKYNAR_DEBUG', false);
+}
+if (!defined('SKYNAR_TIMING')) {
+    define('SKYNAR_TIMING', false);
+}
+define('SKYNAR_MODULE', realpath(__DIR__ . '/mod'));
 
+require_once realpath(realpath(__DIR__) . '/cms/actual/global.php');
+require_once realpath(__DIR__) . '/vendor/autoload.php';
