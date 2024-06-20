bc80dd29457479815fbf70ed173fb57bdac8a1e2
Taras Shkodenko <shkodenko.t@novadigital.com>
2024-06-20 17:31:59 +0300

    Fix auto submit in admin_app.js

--- www/js/admin_app.js
+++ www/js/admin_app.js
@@ -1,27 +1,25 @@
-$('[type=checkbox]').change(function(){
-	$(this).siblings('label').toggleClass('edited')
-	$($(this).prev('input')).val($(this).prop('checked') ? 1 : 0)
-	
-	var toggleDisable = $(this).data('toggledisabled');
-	if(toggleDisable){
-		$(this).prop('checked') ? $(toggleDisable).prop('disabled', false) : $(toggleDisable).prop('disabled', true)
-	}
-})
+$("[type=checkbox]").change(function () {
+  $(this).siblings("label").toggleClass("edited");
+  $($(this).prev("input")).val($(this).prop("checked") ? 1 : 0);
 
-$('[data-toggle="tooltip"]').tooltip()
+  var toggleDisable = $(this).data("toggledisabled");
+  if (toggleDisable) {
+    $(this).prop("checked")
+      ? $(toggleDisable).prop("disabled", false)
+      : $(toggleDisable).prop("disabled", true);
+  }
+});
 
+$('[data-toggle="tooltip"]').tooltip();
 
-$('#entityParamsVotes').on('input change', function(){
-	$('#entityParamsVotesInformer').text($(this).val())
-})
+$("#entityParamsVotes").on("input change", function () {
+  $("#entityParamsVotesInformer").text($(this).val());
+});
 
-$('#frm1').submit(function(e19){
-	console.log('admin_app +19');
-	e19.preventDefault();
+$("#frm1").submit(function (e19) {
+  console.log("admin_app +19");
+  document.querySelector("#mceu_2 > button").click();
 
-	console.log('admin_app +22');
-	document.querySelector('#mceu_2 > button').click();
-
-	console.log('admin_app +25');
-	$('#frm1').submit();
+  console.log("admin_app +22");
+  document.querySelector("#frm1 > input").click();
 });
