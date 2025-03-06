7ea10dafb35243d5af957b95845bc6cd9a1c6296
Oleksii <a.sarnakov@gmail.com>
2025-03-06 01:37:27 +0100

    refactor: migrate from gulp to vite build system

--- foundation/gulpfile.js
+++ foundation/gulpfile.js
@@ -1,115 +1,112 @@
-var gulp = require('gulp'),
-	browserSync = require('browser-sync').create(),
-	$ = require('gulp-load-plugins')(),
-	autoprefixer = require('autoprefixer'),
-	sourcemaps = require('gulp-sourcemaps'),
-	jsImport = require('gulp-js-import'),
-	uglify = require('gulp-uglify'),
-	rigger = require('gulp-rigger');
+var gulp = require("gulp"),
+  browserSync = require("browser-sync").create(),
+  $ = require("gulp-load-plugins")(),
+  autoprefixer = require("autoprefixer"),
+  sourcemaps = require("gulp-sourcemaps"),
+  jsImport = require("gulp-js-import"),
+  uglify = require("gulp-uglify"),
+  rigger = require("gulp-rigger");
 
-const minify = require('gulp-minify');
+const minify = require("gulp-minify");
 
 var sassPaths = [
-	'node_modules/foundation-sites/scss',
-	'node_modules/motion-ui/src',
+  "node_modules/foundation-sites/scss",
+  "node_modules/motion-ui/src",
 ];
 
 function devSass() {
-	return gulp
-		.src([
-			'../www/css/scss/main.scss',
-			'../www/css/scss-v2/style-v2.scss',
-			'node_modules/foundation-sites/scss',
-		])
-		.pipe(sourcemaps.init())
-		.pipe(
-			$.sass({
-				// includePaths: sassPaths,
-				outputStyle: 'compressed', // if css compressed **file size**
-			}).on('error', $.sass.logError),
-		)
-		.pipe(
-			$.postcss([
-				autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] }),
-			]),
-		)
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('../www/css'))
-		.pipe(browserSync.stream());
+  return gulp
+    .src([
+      "../www/css/scss/main.scss",
+      "../www/css/scss-v2/style-v2.scss",
+      // 'node_modules/foundation-sites/scss',
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(
+      $.sass({
+        // includePaths: sassPaths,
+        outputStyle: "compressed", // if css compressed **file size**
+      }).on("error", $.sass.logError)
+    )
+    .pipe(
+      $.postcss([autoprefixer({ browsers: ["last 2 versions", "ie >= 9"] })])
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("../www/css"))
+    .pipe(browserSync.stream());
 }
 
 function sass() {
-	return gulp
-		.src(['scss/app.scss', 'scss/admin_app.scss'])
-		.pipe(sourcemaps.init())
-		.pipe(
-			$.sass({
-				includePaths: sassPaths,
-				outputStyle: 'compressed', // if css compressed **file size**
-			}).on('error', $.sass.logError),
-		)
-		.pipe(
-			$.postcss([
-				autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] }),
-			]),
-		)
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('../www/css'))
-		.pipe(browserSync.stream());
+  return gulp
+    .src(["scss/app.scss", "scss/admin_app.scss"])
+    .pipe(sourcemaps.init())
+    .pipe(
+      $.sass({
+        includePaths: sassPaths,
+        outputStyle: "compressed", // if css compressed **file size**
+      }).on("error", $.sass.logError)
+    )
+    .pipe(
+      $.postcss([autoprefixer({ browsers: ["last 2 versions", "ie >= 9"] })])
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("../www/css"))
+    .pipe(browserSync.stream());
 }
 
 function minifyJs() {
-	return gulp
-		.src('../www/js/common.js', { allowEmpty: true })
-		.pipe(minify({ noSource: true }))
-		.pipe(gulp.dest('../www/js'));
+  return gulp
+    .src("../www/js/common.js", { allowEmpty: true })
+    .pipe(minify({ noSource: true }))
+    .pipe(gulp.dest("../www/js"));
 }
 
 function serve() {
-	browserSync.init({
-		proxy: 'http://slava.skynar.co',
-		port: 3000,
-	});
+  browserSync.init({
+    proxy: "http://slava.skynar.co",
+    port: 3000,
+  });
 
-	gulp.watch('../www/css/scss-v2/components/*.scss', devSass);
-	gulp.watch('../www/css/scss-v2/utils/*.scss', devSass);
-	gulp.watch('../www/css/scss-v2/sections/*.scss', devSass);
-	gulp.watch('../www/css/scss-v2/*.scss', devSass);
+  gulp.watch("../www/css/scss-v2/components/*.scss", devSass);
+  gulp.watch("../www/css/scss-v2/utils/*.scss", devSass);
+  gulp.watch("../www/css/scss-v2/sections/*.scss", devSass);
+  gulp.watch("../www/css/scss-v2/*.scss", devSass);
 
-	gulp.watch('../www/js/common.js', minifyJs);
+  gulp.watch("../www/js/common.js", minifyJs);
 
-	// gulp.watch('../www/css/scss/*.scss', devSass);
-	// gulp.watch('../www/css/scss/partials/*.scss', devSass);
-	// gulp.watch('../www/css/scss/partials/*.css', devSass);
-	gulp.watch('scss/**/*.scss', sass);
-	gulp.watch('js/**/*.js', js);
-	gulp.watch('../template/**/*.tpl').on('change', browserSync.reload);
-	gulp.watch('../www/assets/webpack/*.*').on('change', browserSync.reload);
+  // gulp.watch('../www/css/scss/*.scss', devSass);
+  // gulp.watch('../www/css/scss/partials/*.scss', devSass);
+  // gulp.watch('../www/css/scss/partials/*.css', devSass);
+  gulp.watch("scss/**/*.scss", sass);
+  gulp.watch("js/**/*.js", js);
+  gulp.watch("../template/**/*.tpl").on("change", browserSync.reload);
+  gulp.watch("../www/assets/webpack/*.*").on("change", browserSync.reload);
 }
 
 function js() {
-	return gulp
-		.src(['js/app.js', 'js/admin_app.js'])
-		.pipe(sourcemaps.init())
-		.on('error', swallowError)
-		.pipe(rigger())
-		.pipe(gulp.dest('../www/js/'))
-		.pipe(browserSync.stream());
+  return gulp
+    .src(["js/app.js", "js/admin_app.js"])
+    .pipe(sourcemaps.init())
+    .on("error", swallowError)
+    .pipe(rigger())
+    .pipe(gulp.dest("../www/js/"))
+    .pipe(browserSync.stream());
 }
 
-gulp.task('sass', sass);
-gulp.task('js', js);
-gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series('sass', serve));
+gulp.task("sass", sass);
+gulp.task("js", js);
+gulp.task("serve", gulp.series("sass", serve));
+gulp.task("default", gulp.series("sass", serve));
 
-gulp.task('compress', function() {
-	gulp.src('../www/js/common.js')
-		.pipe(minify({ noSource: true }))
-		.pipe(gulp.dest('../www/js'));
+gulp.task("compress", function () {
+  gulp
+    .src("../www/js/common.js")
+    .pipe(minify({ noSource: true }))
+    .pipe(gulp.dest("../www/js"));
 });
 
 function swallowError(error) {
-	// If you want details of the error in the console
-	console.log(error.toString());
-	this.emit('end');
+  // If you want details of the error in the console
+  console.log(error.toString());
+  this.emit("end");
 }
--- foundation/vite.config.js
+++ foundation/vite.config.js
@@ -1,19 +1,38 @@
 import { defineConfig } from "vite";
 import { minify } from "terser";
+import autoprefixer from "autoprefixer";
+
 export default defineConfig({
   // other config options
 
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({ overrideBrowserslist: ["last 2 versions", "ie >= 9"] }),
+      ],
+    },
+    devSourcemap: true,
+  },
+
   build: {
     minify: true,
-    outDir: "../www/js/dist",
-    assetsDir: "",
+    outDir: "../www",
+    assetsDir: "assets",
     rollupOptions: {
-      input: "../www/app/common.js",
+      input: {
+        common: "../www/app/common.js",
+        "style-v2": "../www/css/scss-v2/style-v2.scss",
+      },
       output: {
         manualChunks: undefined,
-        entryFileNames: `[name]-min.js`,
-        chunkFileNames: `./[name].js`,
-        assetFileNames: `./[name].[ext]`,
+        entryFileNames: `js/dist/[name]-min.js`,
+        chunkFileNames: `js/dist/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith(".css")) {
+            return "css/[name][extname]";
+          }
+          return `assets/[name].[ext]`;
+        },
 
         plugins: [
           {
