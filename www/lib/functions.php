d7b96ed4e807a6db8b989e86beaae4f42735a7f6
Taras Shkodenko <taras.shkodenko@gmail.com>
2025-05-30 18:19:28 +0300

    Fix debug IP in www/lib/functions.php

--- www/lib/functions.php
+++ www/lib/functions.php
@@ -27,7 +27,7 @@ $dotenv->load();
 
 function l_m(string $msg)
 {
-    $debugIp = $_ENV['DEBUG_IP'] ?? '194.88.152.90'; // use debug IP value from env or default one if env is not set
+    $debugIp = $_ENV['DEBUG_IP'] ?? '194.88.153.138'; // use debug IP value from env or default one if env is not set
     error_log(__METHOD__ . ' +' . __LINE__ . ' $debugIp: ' . var_export($debugIp, true));
 
     // Do not log on prod by default
