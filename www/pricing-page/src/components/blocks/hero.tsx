65a6384d980649ff6793815a01f56b220b8c7b28
Oleksii Sarnakov <a.sarnakov@gmail.com>
2025-07-03 23:54:14 +0200

    feat(pricing-page): update Hero component styles and structure for improved responsiveness and visual consistency

--- www/pricing-page/src/components/blocks/hero.tsx
+++ www/pricing-page/src/components/blocks/hero.tsx
@@ -1,11 +1,11 @@
-import { motion } from "motion/react";
-import { CustomBentoGrid } from "./hero-bento-grid";
-import { LampContainer } from "../ui/lamp";
-import { useTranslation } from "../../hooks/useTranslation";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { motion } from "motion/react";
+import { useTranslation } from "../../hooks/useTranslation";
+import { CustomBentoGrid } from "./hero-bento-grid";
 
 export const Hero = () => {
   const { t } = useTranslation();
+  const isMobile = useMediaQuery("(max-width: 1023px)");
 
   const scrollToPricing = () => {
     const pricingSection = document.getElementById("pricing");
@@ -16,11 +16,11 @@ export const Hero = () => {
       });
     }
   };
-  const isMobile = useMediaQuery("(max-width: 1023px)");
 
   return (
-    <LampContainer className="pt-[8rem] lg:pt-[14rem] pb-24">
-      <div className="w-full flex flex-col items-center">
+    <div className="pt-[4rem] ">
+      {/* bg-gradient-to-b from-white from-[5%] to-[#F0F8FF] to-[90%] */}
+      <div className="container  w-full flex flex-col items-center">
         <motion.h1
           initial={{ opacity: 0, y: 70 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -29,7 +29,7 @@ export const Hero = () => {
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className=" bg-gradient-to-br from-slate-100 to-slate-500 py-4 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent md:text-5xl"
+          className="bg-gradient-to-br from-[#8dbbd2] to-sky-900 py-4 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent md:text-5xl"
         >
           {t("hero.title")
             .split("\n")
@@ -57,7 +57,7 @@ export const Hero = () => {
               ease: "easeInOut",
             }}
             onClick={scrollToPricing}
-            className="cursor-pointer mt-8 min-h-12 shadow-[0_4px_14px_0_rgb(50,121,160,39%)] hover:shadow-[0_6px_20px_rgba(50,121,160,50%)] px-6 py-2 bg-white rounded-md text-[#3279a0] font-bold transition duration-200 ease-linear border border-[#3279a0]"
+            className="cursor-pointer mt-8 min-h-12 shadow-[0_4px_14px_0_rgb(50,121,160,39%)] hover:shadow-[0_6px_20px_rgba(50,121,160,50%)] px-6 py-2  rounded-md text-white font-bold transition duration-200 ease-linear bg-[#3279a0]"
           >
             {t("hero.button")}
           </motion.button>
@@ -65,6 +65,6 @@ export const Hero = () => {
 
         <CustomBentoGrid className="w-full mt-12 lg:mt-0" />
       </div>
-    </LampContainer>
+    </div>
   );
 };
